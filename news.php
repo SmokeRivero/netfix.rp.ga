9eba10ac3d2be02b62b710f3b7cc806eaa3904b0
SmokeRivero <110052588+SmokeRivero@users.noreply.github.com>
2022-07-26 20:53:29 +0400

    Add files via upload

---
+++ news.php
@@ -0,0 +1,151 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <title>Samp-Rp — Играй в GTA Онлайн по сети</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="manifest" href="/images/favicon/site.webmanifest">
+    <style>
+        .loaded #preloader{opacity:0;visibility:hidden;-webkit-transition:all .1s ease-out;-o-transition:all .1s ease-out;transition:all .1s ease-out}#preloader{width:100%;height:100%;z-index:9999;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:#fff;position:fixed}#preloader:before{content:"";width:80px;height:80px;display:block;-webkit-animation:rotation 1s linear infinite;animation:rotation 1s linear infinite;border:1px solid #fec400;border-top:1px solid transparent;-webkit-border-radius:100%;border-radius:100%}#preloader .icon{position:absolute;top:50%;left:50%;width:80px;margin-top:-23px;margin-left:-40px;-webkit-animation:wink 1s ease-in-out infinite alternate;animation:wink 1s ease-in-out infinite alternate}@media  only screen and (min-width:768px){#preloader{width:100%;height:100%;z-index:9999;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:#fff;position:fixed}#preloader:before{left:-2px;top:-2px;border-width:2px}}@media  only screen and (min-width:1200px){#preloader{width:100%;height:100%;z-index:9999;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:#fff;position:fixed}}@-webkit-keyframes rotation{from{-webkit-transform:rotate(0);transform:rotate(0)}to{-webkit-transform:rotate(359deg);transform:rotate(359deg)}}@keyframes  rotation{from{-webkit-transform:rotate(0);transform:rotate(0)}to{-webkit-transform:rotate(359deg);transform:rotate(359deg)}}@-webkit-keyframes wink{from{opacity:0}to{opacity:1}}@keyframes  wink{from{opacity:0}to{opacity:1}}
+    </style>
+</head>
+<body>
+<div id="preloader" aria-busy="true" aria-label="Загружается, пожалуйста подождите." role="progressbar"></div>
+<div class="overlay"></div>
+
+<header class="other">
+    <div class="container">
+        <div class="row">
+            <div class="hamburger">
+                <p class="hamburger__text">Меню</p>
+                <div class="hamburger__icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="nav_item-left">
+                    <ul>
+                        <li><a href="https://vk.com/imortaltime" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="13px"> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M12.326,11.794 C12.326,12.262 11.991,12.659 11.534,12.761 C8.491,13.448 6.511,12.561 4.260,9.853 C2.920,8.241 1.533,5.413 0.058,1.336 C-0.129,0.817 0.140,0.245 0.660,0.058 C0.769,0.019 0.884,-0.001 0.999,-0.001 L4.844,-0.001 C5.278,-0.001 5.661,0.283 5.786,0.699 L6.211,2.115 C6.382,2.631 6.102,3.187 5.586,3.358 C5.070,3.529 4.513,3.249 4.342,2.734 C4.336,2.716 4.331,2.698 4.326,2.680 L4.110,1.966 L2.385,1.966 C3.605,5.142 4.743,7.359 5.768,8.592 C7.384,10.537 8.564,11.202 10.381,10.972 C10.660,8.921 12.002,8.056 13.970,9.010 C14.091,9.069 14.202,9.158 14.288,9.262 L15.583,10.817 L17.536,10.817 C17.248,10.216 16.986,9.781 16.758,9.522 C16.698,9.454 16.637,9.387 16.574,9.322 L16.357,9.093 C15.114,7.784 14.709,6.760 15.679,5.430 C16.660,4.088 17.340,2.927 17.725,1.966 L15.778,1.966 C14.298,5.398 13.079,7.242 11.805,7.652 C11.276,7.823 10.709,7.532 10.538,7.004 C10.506,6.904 10.489,6.800 10.489,6.695 L10.489,4.492 C10.488,3.941 10.935,3.493 11.487,3.492 C11.922,3.491 12.308,3.771 12.441,4.185 C12.937,3.329 13.522,2.132 14.157,0.614 C14.311,0.247 14.670,-0.001 15.069,-0.001 L18.999,-0.001 C19.552,-0.001 19.999,0.446 19.999,0.997 C19.999,1.050 19.995,1.102 19.987,1.153 C19.750,2.651 18.836,4.455 17.260,6.613 C17.069,6.875 17.130,7.030 17.767,7.702 L17.987,7.933 C18.077,8.028 18.150,8.107 18.218,8.185 C18.781,8.824 19.338,9.894 19.932,11.424 C20.132,11.939 19.876,12.517 19.361,12.716 C19.245,12.761 19.123,12.784 18.999,12.784 L15.071,12.784 C14.779,12.784 14.502,12.654 14.315,12.430 L12.870,10.695 C12.358,10.470 12.326,10.619 12.326,11.794 ZM9.057,1.966 L9.175,6.566 C9.169,7.108 8.723,7.544 8.179,7.537 C7.664,7.532 7.240,7.130 7.208,6.615 L7.063,1.007 C7.049,0.464 7.479,0.013 8.023,-0.001 C8.031,-0.001 8.039,-0.001 8.048,-0.001 L11.629,-0.001 C12.172,-0.001 12.613,0.439 12.613,0.982 C12.613,1.525 12.172,1.966 11.629,1.966 L9.058,1.966 L9.057,1.966 Z"></path></svg>
+                            </a></li>
+                        <li><a href="#" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="16px"> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M7.486,5.856 C7.002,5.589 6.826,4.981 7.092,4.497 C7.359,4.013 7.967,3.838 8.451,4.104 C8.472,4.116 8.493,4.128 8.514,4.142 L13.514,7.142 C13.987,7.427 14.140,8.041 13.856,8.514 C13.771,8.654 13.654,8.772 13.514,8.856 L8.514,11.856 C8.040,12.140 7.426,11.986 7.142,11.512 C7.049,11.357 7.000,11.180 7.000,10.999 L7.000,7.999 C7.000,7.447 7.448,6.999 8.000,6.999 C8.552,6.999 9.000,7.447 9.000,7.999 L9.000,9.233 L11.056,7.999 L7.486,5.856 ZM2.000,11.999 C2.000,13.103 2.895,13.999 4.000,13.999 L16.000,13.999 C17.105,13.999 18.000,13.103 18.000,11.999 L18.000,3.999 C18.000,2.894 17.105,1.999 16.000,1.999 L4.000,1.999 C2.895,1.999 2.000,2.894 2.000,3.999 L2.000,11.999 ZM-0.000,11.999 L-0.000,3.999 C-0.000,1.790 1.791,-0.001 4.000,-0.001 L16.000,-0.001 C18.209,-0.001 20.000,1.790 20.000,3.999 L20.000,11.999 C20.000,14.208 18.209,15.999 16.000,15.999 L4.000,15.999 C1.791,15.999 -0.000,14.208 -0.000,11.999 Z"></path></svg>
+                            </a></li>
+                        <li><a href="#" target="_blank">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"x="0px" y="0px" width="20px" height="20px" viewBox="0 0 382.3 332.2" style="enable-background:new 0 0 382.3 332.2;" xml:space="preserve"><defs> </defs> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M353.3,20.8l-1.3-3.2c0-0.1-0.1-0.2-0.2-0.2l-3.1-1.4C322.9,4.2,286.4,1.3,254.2,0h-36.3 c-0.4,0-0.5,0.6-0.2,0.7l74.6,35.8c0.4,0.2,0.2,0.8-0.3,0.7C258.5,28.4,224.6,24,191.1,24c-33.4,0-67.1,4.4-100.6,13.1 c-0.5,0.1-0.7-0.5-0.3-0.7l77.2-35.7c0.4-0.2,0.3-0.7-0.2-0.7h-39.3C95.9,1.3,59.4,4.2,33.6,15.9l-3.1,1.4c-0.1,0-0.2,0.1-0.2,0.2 L29,20.8C2.5,87.7-5,201.5,3,272.7l0.4,3.7l3,2.3c41.3,32,96.5,48.5,131.3,52.9l4.5,0.6c1.5,0.2,2.9-0.7,3.4-2l22.6-60.4 c0.7-1.9-0.5-3.9-2.5-4.3l-6.9-1.1c-24.2-3.8-58.5-13.9-87.4-40.1c-0.6-0.6,0-1.6,0.8-1.3c2.4,0.9,4.9,1.9,7.6,2.9 c6.6,2.6,13.3,5.2,17.7,6.3c31.1,7.4,62.6,11.1,93.7,11.1v-0.6h0.1l0,0.6c31.1,0,62.6-3.7,93.6-11.1c4.4-1.1,11.2-3.7,17.7-6.3 c2.7-1,5.3-2.1,7.6-2.9c0.8-0.3,1.4,0.7,0.8,1.3c-28.9,26.2-63.2,36.2-87.4,40l-6.9,1.1c-2,0.3-3.2,2.4-2.5,4.2l22.6,60.6 c0.5,1.4,1.9,2.2,3.4,2l4.5-0.6c34.7-4.4,90-20.8,131.3-52.9l3-2.3l0.4-3.7C387.2,201.5,379.7,87.7,353.3,20.8z M117.3,185.2 c-28.1,2.5-51.5-20.9-49-49c1.9-21.5,19.3-38.9,40.8-40.8c28.1-2.5,51.5,20.9,49,49C156.2,166,138.9,183.3,117.3,185.2z M273.2,185.2c-28.1,2.5-51.5-20.9-49-49c1.9-21.5,19.3-38.9,40.8-40.8c28.1-2.5,51.5,20.9,49,49C312,166,294.7,183.3,273.2,185.2z"/> </svg>
+                            </a></li>
+                        <li><a href="#" target="_blank">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129" width="21px" height="21px"> <g> <g> <path fill-rule="evenodd" opacity="0.902" fill="rgb(255, 255, 255)" d="m68.3,85.6c15.3,0 27.7-12.5 27.7-27.8v-2.7-20.9c0-15.3-12.4-27.7-27.7-27.7-15.3,0-27.7,12.4-27.7,27.7v20.9 2.7c0,15.3 12.4,27.8 27.7,27.8zm-19.5-51.4c0-10.8 8.8-19.6 19.6-19.6 10.8,0 19.6,8.8 19.6,19.6v20.9c0,10.8-8.8,19.6-19.6,19.6-10.8,0-19.6-8.8-19.6-19.6v-20.9z"/> <path fill-rule="evenodd" opacity="0.902" fill="rgb(255, 255, 255)" d="m108,57.8c0-2.3-1.8-4.1-4.1-4.1s-4.1,1.8-4.1,4.1c0,17.4-14.1,31.5-31.5,31.5s-31.5-14.1-31.5-31.5c0-2.3-1.8-4.1-4.1-4.1s-4.1,1.8-4.1,4.1c0,20.5 15.6,37.4 35.6,39.5v17.1h-22.9c-2.3,0-4.1,1.8-4.1,4.1 0,2.3 1.8,4.1 4.1,4.1h52.3c2.3,0 4.1-1.8 4.1-4.1 0-2.3-1.8-4.1-4.1-4.1h-21.2v-17.1c20-2.1 35.6-19 35.6-39.5z"/> </g> </g> </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <ul class="nav_item-list">
+                    <li><a href="/">Главная</a></li>
+                    <!--<li><a href="#about" class="scrollto">О проекте</a></li>-->
+                    <li><a href="#">Форум</a></li>
+                    <li class="logotype"></li>
+                    <li><a href="#">Донат</a></li>
+                    <li><a href="/news.php">Новости</a></li>
+                </ul>
+                <div class="nav_item-right">
+                    <ul>
+                        <li class="dev"><a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px"> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M9.000,6.001 C10.105,6.001 11.000,5.106 11.000,4.001 C11.000,2.897 10.105,2.001 9.000,2.001 C7.895,2.001 7.000,2.897 7.000,4.001 C7.000,5.106 7.895,6.001 9.000,6.001 ZM11.520,7.107 L12.530,8.117 L16.292,9.266 C16.661,9.379 16.932,9.694 16.988,10.076 L17.989,16.854 C18.070,17.400 17.692,17.908 17.146,17.989 C17.098,17.996 17.049,18.000 17.000,18.000 L1.005,18.000 C0.453,18.000 0.005,17.553 0.005,17.000 C0.005,16.952 0.008,16.903 0.015,16.855 L1.011,10.078 C1.062,9.727 1.295,9.430 1.623,9.297 L4.623,8.074 C5.139,7.876 5.717,8.133 5.915,8.649 C6.108,9.150 5.870,9.714 5.377,9.926 L2.907,10.933 L2.162,16.000 L15.840,16.000 L15.100,10.994 L11.705,9.956 C11.549,9.908 11.407,9.822 11.291,9.707 L9.547,7.963 C7.054,8.308 4.781,6.276 5.014,3.600 C5.143,2.123 6.145,0.799 7.532,0.274 C10.044,-0.677 12.616,0.960 12.961,3.452 C13.153,4.839 12.606,6.225 11.519,7.107 L11.520,7.107 Z"/></svg>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="popup-dev">
+                    Личный кабинет закрыт.<br>
+                    Сделаю личный кабинет/донат за отдельную плату <a href="https://vk.com/imortaltime" target="_blank">пишите в ЛС</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="headline">
+                <h2>
+                    Новости <span>проекта</span>
+                </h2>
+            </div>
+            <div class="news_item">
+                <div class="news_item-type">
+                    <div class="news_item-type__wallpaper">
+                        <div class="news_item-type__wallpaper-img"><img src="http://a0304461.xsph.ru/media/37/-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.png" alt=""></div>
+                    </div>
+                    <div class="news_item-type__desc">
+                        <h2>Обновление за 01.01.2020</h2>
+                        <p>Текст обновления</p>
+                        <a href="/news/obnovlenie-za-28-06-2019" class="news_item-type__desc-btn">
+                            Читать новость
+                            <i><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 31.49 31.49" xml:space="preserve"> <path d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111 C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587 c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g></g><g></g><g></g><g></g><g></g><g></g></svg></i>
+                        </a>
+                    </div>
+                </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="logotype"></div>
+            <ul class="footer_nav">
+                <li><a href="/">Главная</a></li>
+                <li><a href="/#about" class="scrollto">О проекте</a></li>
+                <li><a href="http://beta.samp-rp.ru/донат">Донат</a></li>
+                <li><a href="/news.php">Новости</a></li>
+                <li><a href="https://vk.com/imortaltime" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="13px"> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M12.326,11.794 C12.326,12.262 11.991,12.659 11.534,12.761 C8.491,13.448 6.511,12.561 4.260,9.853 C2.920,8.241 1.533,5.413 0.058,1.336 C-0.129,0.817 0.140,0.245 0.660,0.058 C0.769,0.019 0.884,-0.001 0.999,-0.001 L4.844,-0.001 C5.278,-0.001 5.661,0.283 5.786,0.699 L6.211,2.115 C6.382,2.631 6.102,3.187 5.586,3.358 C5.070,3.529 4.513,3.249 4.342,2.734 C4.336,2.716 4.331,2.698 4.326,2.680 L4.110,1.966 L2.385,1.966 C3.605,5.142 4.743,7.359 5.768,8.592 C7.384,10.537 8.564,11.202 10.381,10.972 C10.660,8.921 12.002,8.056 13.970,9.010 C14.091,9.069 14.202,9.158 14.288,9.262 L15.583,10.817 L17.536,10.817 C17.248,10.216 16.986,9.781 16.758,9.522 C16.698,9.454 16.637,9.387 16.574,9.322 L16.357,9.093 C15.114,7.784 14.709,6.760 15.679,5.430 C16.660,4.088 17.340,2.927 17.725,1.966 L15.778,1.966 C14.298,5.398 13.079,7.242 11.805,7.652 C11.276,7.823 10.709,7.532 10.538,7.004 C10.506,6.904 10.489,6.800 10.489,6.695 L10.489,4.492 C10.488,3.941 10.935,3.493 11.487,3.492 C11.922,3.491 12.308,3.771 12.441,4.185 C12.937,3.329 13.522,2.132 14.157,0.614 C14.311,0.247 14.670,-0.001 15.069,-0.001 L18.999,-0.001 C19.552,-0.001 19.999,0.446 19.999,0.997 C19.999,1.050 19.995,1.102 19.987,1.153 C19.750,2.651 18.836,4.455 17.260,6.613 C17.069,6.875 17.130,7.030 17.767,7.702 L17.987,7.933 C18.077,8.028 18.150,8.107 18.218,8.185 C18.781,8.824 19.338,9.894 19.932,11.424 C20.132,11.939 19.876,12.517 19.361,12.716 C19.245,12.761 19.123,12.784 18.999,12.784 L15.071,12.784 C14.779,12.784 14.502,12.654 14.315,12.430 L12.870,10.695 C12.358,10.470 12.326,10.619 12.326,11.794 ZM9.057,1.966 L9.175,6.566 C9.169,7.108 8.723,7.544 8.179,7.537 C7.664,7.532 7.240,7.130 7.208,6.615 L7.063,1.007 C7.049,0.464 7.479,0.013 8.023,-0.001 C8.031,-0.001 8.039,-0.001 8.048,-0.001 L11.629,-0.001 C12.172,-0.001 12.613,0.439 12.613,0.982 C12.613,1.525 12.172,1.966 11.629,1.966 L9.058,1.966 L9.057,1.966 Z"/></svg>
+                    </a></li>
+                <li><a href="ССЫЛКА НА ВАШ РОЛИК" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="16px"> <path fill-rule="evenodd"  opacity="0.902" fill="rgb(255, 255, 255)" d="M7.486,5.856 C7.002,5.589 6.826,4.981 7.092,4.497 C7.359,4.013 7.967,3.838 8.451,4.104 C8.472,4.116 8.493,4.128 8.514,4.142 L13.514,7.142 C13.987,7.427 14.140,8.041 13.856,8.514 C13.771,8.654 13.654,8.772 13.514,8.856 L8.514,11.856 C8.040,12.140 7.426,11.986 7.142,11.512 C7.049,11.357 7.000,11.180 7.000,10.999 L7.000,7.999 C7.000,7.447 7.448,6.999 8.000,6.999 C8.552,6.999 9.000,7.447 9.000,7.999 L9.000,9.233 L11.056,7.999 L7.486,5.856 ZM2.000,11.999 C2.000,13.103 2.895,13.999 4.000,13.999 L16.000,13.999 C17.105,13.999 18.000,13.103 18.000,11.999 L18.000,3.999 C18.000,2.894 17.105,1.999 16.000,1.999 L4.000,1.999 C2.895,1.999 2.000,2.894 2.000,3.999 L2.000,11.999 ZM-0.000,11.999 L-0.000,3.999 C-0.000,1.790 1.791,-0.001 4.000,-0.001 L16.000,-0.001 C18.209,-0.001 20.000,1.790 20.000,3.999 L20.000,11.999 C20.000,14.208 18.209,15.999 16.000,15.999 L4.000,15.999 C1.791,15.999 -0.000,14.208 -0.000,11.999 Z"/></svg>
+                    </a></li>
+            </ul>
+            <div class="copyright">
+                SAMP-RP.RU © 2010-2020 г.<br>Все права защищены!
+        </div>
+    </div>
+</footer>
+
+<link rel="stylesheet" href="/css/main.min.css?id=atc92ffb5522e0de9c9ed2">
+<link rel="stylesheet" href="/css/fonts.css?ver=bbs8fds873jhfhe3ed3">
+<script src="/js/jquery.min.js"></script>
+<script src="/js/fancybox.min.js"></script>
+<script src="/js/swiper.min.js"></script>
+<script src="/js/slick.min.js"></script>
+<script src="/js/index.min.js?id=9f92e9f5b08abb7f160"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(53773417, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/53773417" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?160",t.onload=function(){VK.Retargeting.Init("VK-RTRG-372922-ggfo8"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-372922-ggfo8" style="position:fixed; left:-999px;" alt=""/></noscript>
+</body>
+</html>
